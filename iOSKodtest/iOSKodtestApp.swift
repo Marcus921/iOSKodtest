747d7691d106d67af9c33124b52c630b9d5eb928
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2023-11-29 09:14:52 +0100

    Initial Commit

---
+++ iOSKodtest/iOSKodtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSKodtestApp.swift
+//  iOSKodtest
+//
+//  Created by Marcus Malmgren on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct iOSKodtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
